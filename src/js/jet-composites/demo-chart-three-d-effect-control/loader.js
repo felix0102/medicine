c86ca493bd4123de8a7d7ca52718175e8baa65dd
felix <felix.liu@foxmail.com>
2018-04-17 19:52:48 +0800

    ignore

--- src/js/jet-composites/demo-chart-three-d-effect-control/loader.js
+++
@@ -1,8 +0,0 @@
-define(['ojs/ojcore', 'text!./view.html', 'text!./component.json', 'css!./styles', 'ojs/ojcomposite', 'ojs/ojbutton'],
-  function(oj, view, metadata) {
-    oj.Composite.register('demo-chart-three-d-effect-control', {
-      view: {inline: view}, 
-      metadata: {inline: JSON.parse(metadata)}
-    });
-  }
-);
--- src/js/jet-composites/my-bar/README.md
+++
@@ -1,1 +0,0 @@
-ref-value="[{'text': '参考值', 'type': 'line', 'value': 20, color: '#FAD55C', 'displayInLegend': 'on', 'lineWidth': 1, 'location': 'back', 'shortDesc': '参考值'}]" 
